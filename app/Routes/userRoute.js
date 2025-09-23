266a03f1633288f1fa2318597d61c5f52a5a65e0
Cyprian Obi <obicyprian180@gmail.com>
2025-09-23 19:24:31 +0100

    files update

---
+++ app/Routes/userRoute.js
@@ -0,0 +1,10 @@
+const {Register, logUserIn, logUserOut} = require('../app/controllers/userController')
+const express = require ('express')
+
+const Route = express.Router()
+
+Route.post('/register', Register)
+Route.post('/login', logUserIn)
+Route.get('/logout', logUserOut)
+
+module.exports = Route
